ecd1184327ea8fcc7f602fce74337ac1607cc1a3
Rodrigo Fernandes <rfernandes@digital-works.com>
2021-03-31 00:21:14 +0100

    Added chatdata.json and an endpoint that reads the question and gives an answer for chatdata.json

--- api/server.js
+++ api/server.js
@@ -1,6 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const bodyparser = require("body-parser");
+const app = express();
+const fs = require("fs");
 
-app.get('/api/', (req, res) => res.send('Hello Everyione'))
+const fileraw = fs.readFileSync(__dirname + "/data/chatdata.json");
+const file = JSON.parse(fileraw);
+const defaultAnwer = file.default;
+const questionsList = Object.values(file.questionlist);
 
-app.listen(3000, () => console.log("Connected to 3000"))
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.get("/api/", function (req, res) {
+  res.send("We live bros!");
+});
+
+app.post("/api/getAnswers", function (req, res) {
+  let possibleAnswers = questionsList.find((currentvalue) => {
+    let cenas = currentvalue.questions.find((value) => {
+      if (value == req.body.question) return currentvalue.answer;
+    });
+
+    if (cenas) return cenas;
+  });
+  if (possibleAnswers) res.send(possibleAnswers.answer);
+  else res.send(defaultAnwer);
+
+});
+
+app.listen(3000, () => console.log("Connected to 3000"));
