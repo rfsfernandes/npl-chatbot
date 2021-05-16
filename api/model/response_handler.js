75b87a23566c08a028cb986b0e0db34733897fa3
Rodrigo Fernandes <rodrigoserrafernandes98@gmail.com>
2021-05-16 21:27:11 +0100

    Fixed some bugs

--- api/model/response_handler.js
+++ api/model/response_handler.js
@@ -61,47 +61,47 @@ module.exports = class ResponseHandler {
 
     //console.log(intent);
     if (intent.name != "confirmation") {
-      this.current_state = intent.name;
+      current_state = intent.name;
     }
 
-    //console.log(this.current_state);
+    //console.log(current_state);
 
     switch (
-      this.current_state == undefined ? intent.name : this.current_state
+      current_state == undefined ? intent.name : current_state
     ) {
       case "room_reservation":
-        this.message = this.handleReservation(res, req, entities);
+        message = this.handleReservation(res, req, entities);
         break;
       case "room_change":
-        this.message = this.handleReservationChange(res, req, entities);
+        message = this.handleReservationChange(res, req, entities);
         break;
       case "room_cancel":
-        this.message = this.handleReservationCancel(res, req, entities);
+        message = this.handleReservationCancel(res, req, entities);
         break;
       case "gym_access":
-        this.message = this.handleGymAccess(res, req, entities);
+        message = this.handleGymAccess(res, req, entities);
         break;
       case "pool_access":
-        this.message = this.handlePoolAccess(res, req, entities);
+        message = this.handlePoolAccess(res, req, entities);
         break;
       case "greetings":
-        this.message = this.handleGreetings(res, req, entities);
+        message = this.handleGreetings(res, req, entities);
         break;
     }
 
-    return this.message;
+    return message;
   };
 
   handleGreetings = (res, req, entities) =>{
-    this.message = this.file.greetings.greetings
+    message = this.file.greetings.greetings
     [
       this.getRandom(this.file.greetings.greetings.length)
     ];
-    return this.message;
+    return message;
   }
 
   handleReservation = (res, req, entities) => {
-    this.temp_reservation = new Reservation();
+    temp_reservation = new Reservation();
     let reservationQuestion = false;
 
     if (
@@ -110,7 +110,7 @@ module.exports = class ResponseHandler {
         entities.hasOwnProperty("bed_quantity:bed_quantity") ||
         entities.hasOwnProperty("bed_type:bed_type"))
     ) {
-      this.message = this.file.room_reservation.reservation_prices[
+      message = this.file.room_reservation.reservation_prices[
         this.getRandom(this.file.room_reservation.reservation_prices.length)
       ]
         .replace("{0}", couple_bed_price)
@@ -143,9 +143,9 @@ module.exports = class ResponseHandler {
 
         let cookie_room_reservation = this.readCookie(req, "room_reservation");
 
-        if (cookie_room_reservation != undefined) {
+        if (cookie_room_reservation && cookie_room_reservation != "undefined") {
           this.reservation = cookie_room_reservation;
-          this.message =
+          message =
             this.file.room_reservation.room_already_reserved[
               this.getRandom(
                 this.file.room_reservation.room_already_reserved.length
@@ -162,7 +162,7 @@ module.exports = class ResponseHandler {
                     )
                   ]
                 );
-                this.message =
+                message =
                   this.file.room_reservation.missing_confirmation[
                     this.getRandom(
                       this.file.room_reservation.missing_confirmation.length
@@ -175,7 +175,7 @@ module.exports = class ResponseHandler {
                       : single_bed_price * parseInt(this.reservation.bed_quantity)) +
                   "€";
               } else {
-                this.message =
+                message =
                   this.file.room_reservation[`missing_${key}`][
                     this.getRandom(
                       this.file.room_reservation[`missing_${key}`].length
@@ -206,14 +206,14 @@ module.exports = class ResponseHandler {
               this.reservation.bed_ids = bed_ids_string;
               this.saveCookie(res, "room_reservation", this.reservation);
 
-              this.message =
+              message =
                 this.file.room_reservation.reservation_success[
                   this.getRandom(
                     this.file.room_reservation.reservation_success.length
                   )
                 ] + this.getResume();
             } else {
-              this.message =
+              message =
                 this.file.room_reservation.reservation_fail[
                   this.getRandom(
                     this.file.room_reservation.reservation_fail.length
@@ -223,29 +223,29 @@ module.exports = class ResponseHandler {
           }
         }
       } else {
-        this.message = this.file.room_reservation.room_reservation_question;
+        message = this.file.room_reservation.room_reservation_question;
       }
     }
 
-    return this.message;
+    return message;
   };
 
   handleReservationChange = (res, req, entities) => {
     let cookie_room_reservation = this.readCookie(req, "room_reservation");
 
-    if (cookie_room_reservation != undefined) {
+    if (cookie_room_reservation && cookie_room_reservation != "undefined") {
       this.reservation = cookie_room_reservation;
 
       if (Object.keys(entities).length === 0) {
-        if (!this.change_question_made) {
-          this.message =
+        if (!change_question_made) {
+          message =
             this.file.room_change.room_change_intro[
               this.getRandom(this.file.room_change.room_change_intro.length)
             ];
-          this.change_question_made = true;
+          change_question_made = true;
         }
       } else {
-        this.change_question_made = false;
+        change_question_made = false;
 
         for (let key in entities) {
           if (entities[key][0].role == "confirmation") {
@@ -254,73 +254,73 @@ module.exports = class ResponseHandler {
               value = true;
             }
 
-            this.temp_reservation.confirmation = value;
+            temp_reservation.confirmation = value;
           } else {
-            this.temp_reservation[entities[key][0].role] =
+            temp_reservation[entities[key][0].role] =
               entities[key][0].value;
           }
         }
 
-        if (this.temp_reservation.datetime != undefined) {
-          const date = new Date(this.temp_reservation.datetime);
-          this.temp_reservation.datetime = date;
+        if (temp_reservation.datetime != undefined) {
+          const date = new Date(temp_reservation.datetime);
+          temp_reservation.datetime = date;
         }
 
-        if (this.temp_reservation.confirmation != undefined) {
-          if (this.temp_reservation.confirmation) {
+        if (temp_reservation.confirmation != undefined) {
+          if (temp_reservation.confirmation) {
             console.log("Confirmation true");
             for (const key in this.reservation) {
-              if (this.temp_reservation[key] != undefined) {
-                this.reservation[key] = this.temp_reservation[key];
+              if (temp_reservation[key] != undefined) {
+                this.reservation[key] = temp_reservation[key];
               }
             }
 
             this.saveCookie(res, "room_reservation", this.reservation);
-            this.temp_reservation = new Reservation();
-            this.message =
+            temp_reservation = new Reservation();
+            message =
               this.file.room_change.change_success[
                 this.getRandom(this.file.room_change.change_success.length)
               ];
           } else {
-            this.message =
+            message =
               this.file.room_change.change_fail[
                 this.getRandom(this.file.room_change.change_fail.length)
               ];
           }
         } else {
           if (
-            (this.temp_reservation.bed_quantity != undefined &&
-              (this.temp_reservation.bed_quantity >
+            (temp_reservation.bed_quantity != undefined &&
+              (temp_reservation.bed_quantity >
                 this.reservation.bed_quantity ||
-                this.temp_reservation.bed_quantity <
+                temp_reservation.bed_quantity <
                   this.reservation.bed_quantity)) ||
-            (this.temp_reservation.bed_type != undefined &&
-              ((this.temp_reservation.bed_type.includes("casal") &&
+            (temp_reservation.bed_type != undefined &&
+              ((temp_reservation.bed_type.includes("casal") &&
                 !this.reservation.bed_type.includes("casal")) || // casal
-                (!this.temp_reservation.bed_type.includes("casal") &&
+                (!temp_reservation.bed_type.includes("casal") &&
                   this.reservation.bed_type.includes("casal")) ||
-                ((this.temp_reservation.bed_type.includes("individual") ||
-                  this.temp_reservation.bed_type.includes("individuais")) &&
+                ((temp_reservation.bed_type.includes("individual") ||
+                  temp_reservation.bed_type.includes("individuais")) &&
                   (!this.reservation.bed_type.includes("individual") ||
                     !this.reservation.bed_type.includes("individuais"))) || // individual
-                ((!this.temp_reservation.bed_type.includes("individual") ||
-                  !this.temp_reservation.bed_type.includes("individuais")) &&
+                ((!temp_reservation.bed_type.includes("individual") ||
+                  !temp_reservation.bed_type.includes("individuais")) &&
                   (this.reservation.bed_type.includes("individual") ||
                     this.reservation.bed_type.includes("individuais")))))
           ) {
-            this.message = this.file.room_change.confirming_changes[
+            message = this.file.room_change.confirming_changes[
               this.getRandom(this.file.room_change.confirming_changes.length)
             ].replace("{0}", this.getResume());
           } else if (
-            this.temp_reservation.datetime != this.reservation.datetime
+            temp_reservation.datetime != this.reservation.datetime
           ) {
-            this.message = this.file.room_change.date_change[
+            message = this.file.room_change.date_change[
               this.getRandom(this.file.room_change.date_change.length)
             ]
               .replace("{0}", this.formatDate(this.reservation.datetime))
-              .replace("{1}", this.formatDate(this.temp_reservation.datetime));
+              .replace("{1}", this.formatDate(temp_reservation.datetime));
           } else {
-            this.message =
+            message =
               this.file.room_change.no_changes_identified[
                 this.getRandom(
                   this.file.room_change.no_changes_identified.length
@@ -330,10 +330,10 @@ module.exports = class ResponseHandler {
         }
       }
     } else {
-      this.message = this.file.room_change.room_not_reserved_yet;
+      message = this.file.room_change.room_not_reserved_yet;
     }
 
-    return this.message;
+    return message;
   };
 
   handleReservationCancel = (res, req, entities) => {
@@ -342,7 +342,7 @@ module.exports = class ResponseHandler {
     if (cookie_room_reservation && cookie_room_reservation != "undefined") {
       this.reservation = cookie_room_reservation;
 
-      if (this.waiting_cancel) {
+      if (waiting_cancel) {
         let cancel = false;
         for (let key in entities) {
           if (entities[key][0].role == "confirmation") {
@@ -353,32 +353,33 @@ module.exports = class ResponseHandler {
         }
 
         if (cancel) {
-          this.message =
+          message =
             this.file.room_cancel.cancel_success[
               this.getRandom(this.file.room_cancel.cancel_success.length)
             ];
+          this.reservation = new Reservation();
           this.saveCookie(res, "room_reservation", undefined);
         } else {
-          this.message =
+          message =
             this.file.room_cancel.cancel_fail[
               this.getRandom(this.file.room_cancel.cancel_fail.length)
             ];
         }
-        this.waiting_cancel = false;
+        waiting_cancel = false;
       } else {
-        this.message = this.file.room_cancel.cancel_confirmation[
+        message = this.file.room_cancel.cancel_confirmation[
           this.getRandom(this.file.room_cancel.cancel_confirmation.length)
         ].replace("{0}", this.getResume());
-        this.waiting_cancel = true;
+        waiting_cancel = true;
       }
     } else {
-      this.message =
+      message =
         this.file.room_cancel.no_reservation_found[
           this.getRandom(this.file.room_cancel.no_reservation_found.length)
         ];
     }
 
-    return this.message;
+    return message;
   };
 
   handleGymAccess = (res, req, entities) => {
@@ -399,22 +400,22 @@ module.exports = class ResponseHandler {
     }
 
     if ((gym && price) || price) {
-      this.message =
+      message =
         this.file.gym_access.access_price[
           this.getRandom(this.file.gym_access.access_price.length)
         ];
     } else if ((gym && obj) || obj) {
-      this.message =
+      message =
         this.file.gym_access.gym_machines[
           this.getRandom(this.file.gym_access.gym_machines.length)
         ];
     } else if (gym) {
-      this.message =
+      message =
         this.file.gym_access.gym[
           this.getRandom(this.file.gym_access.gym.length)
         ];
     }
-    return this.message;
+    return message;
   };
 
   handlePoolAccess = (res, req, entities) => {
@@ -435,22 +436,22 @@ module.exports = class ResponseHandler {
     }
 
     if ((pool && price) || price) {
-      this.message =
+      message =
         this.file.pool_access.access_price[
           this.getRandom(this.file.pool_access.access_price.length)
         ];
     } else if ((pool && obj) || obj) {
-      this.message =
+      message =
         this.file.pool_access.pool_machines[
           this.getRandom(this.file.pool_access.pool_machines.length)
         ];
     } else if (pool) {
-      this.message =
+      message =
         this.file.pool_access.pool[
           this.getRandom(this.file.pool_access.pool.length)
         ];
     }
-    return this.message;
+    return message;
   };
 
   saveCookie = (res, cookieName, cookieValue) => {
